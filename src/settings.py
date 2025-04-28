1274ddd1c354c3f757c90f5675043bd03982aff3
gmonitor <seregin.da@didenokteam.com>
2025-04-28 12:45:54 +0300

    feat: settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,13 +2,20 @@ import logging
 
 from pydantic_settings import BaseSettings
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
 
 class Settings(BaseSettings):
     groq_token: str = "token"
+    gigachat_token: str = "token"
+
     kafka_host: str = "localhost"
     kafka_port: int = 9092
+
+    aws_host: str = "host"
+    aws_bucket_name: str = "bucket"
+    aws_access_key_id: str = "access_key_id"
+    aws_secret_access_key: str = "secret_access_key"
 
 
 settings = Settings()
